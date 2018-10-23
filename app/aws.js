6e1d3d54fb7ad3589800dd2abfd372520c94bfc1
Peter Liu <peterliu@me.com>
2018-10-23 16:15:39 +1100

    AWS NodeJS Libraries

---
+++ app/aws.js
@@ -0,0 +1,28 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+
+// Create unique bucket name
+var bucketName = 'node-sdk-sample-' + uuid.v4();
+
+// Create name for uploaded object key
+var keyName = 'hello_world.txt';
+
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise.then(
+  function(data) {
+    // Create params for putObject call
+    var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    // Create object upload promise
+    var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+    uploadPromise.then(
+      function(data) {
+        console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+      });
+}).catch(
+  function(err) {
+    console.error(err, err.stack);
+});
